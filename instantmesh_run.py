be6d36e1acec2b19e648995090d589b1c09d5fd1
Julez0910 <Julez0910>
2025-07-17 22:10:45 +0200

    add run script for InstantMesh

---
+++ instantmesh_run.py
@@ -0,0 +1,206 @@
+import os
+import numpy as np
+import torch
+import rembg
+from PIL import Image
+from torchvision.transforms import v2
+from pytorch_lightning import seed_everything
+from omegaconf import OmegaConf
+from einops import rearrange, repeat
+from tqdm import tqdm
+from huggingface_hub import hf_hub_download
+from diffusers import DiffusionPipeline, EulerAncestralDiscreteScheduler
+import sys
+from pathlib import Path
+
+instantmesh_base = Path("external") / "InstantMesh"
+IM_ROOT = Path(__file__).resolve().parent / "external" / "InstantMesh"
+sys.path.insert(0, str(IM_ROOT))
+sys.path.insert(0, str(IM_ROOT / "src"))
+
+from external.InstantMesh.src.utils.train_util import instantiate_from_config
+from external.InstantMesh.src.utils.camera_util import (
+    FOV_to_intrinsics, 
+    get_zero123plus_input_cameras,
+    get_circular_camera_poses,
+)
+from external.InstantMesh.src.utils.mesh_util import save_obj, save_obj_with_mtl
+from external.InstantMesh.src.utils.infer_util import remove_background, resize_foreground
+
+output_base = Path("outputs") / "instantmesh"
+input_base = Path("images")
+
+create_texture_map = True
+
+seed_everything(42)
+
+def get_render_cameras(batch_size=1, M=120, radius=4.0, elevation=20.0, is_flexicubes=False):
+    """
+    Get the rendering camera parameters.
+    """
+    c2ws = get_circular_camera_poses(M=M, radius=radius, elevation=elevation)
+    if is_flexicubes:
+        cameras = torch.linalg.inv(c2ws)
+        cameras = cameras.unsqueeze(0).repeat(batch_size, 1, 1, 1)
+    else:
+        extrinsics = c2ws.flatten(-2)
+        intrinsics = FOV_to_intrinsics(30.0).unsqueeze(0).repeat(M, 1, 1).float().flatten(-2)
+        cameras = torch.cat([extrinsics, intrinsics], dim=-1)
+        cameras = cameras.unsqueeze(0).repeat(batch_size, 1, 1)
+    return cameras
+
+
+def render_frames(model, planes, render_cameras, render_size=512, chunk_size=1, is_flexicubes=False):
+    """
+    Render frames from triplanes.
+    """
+    frames = []
+    for i in tqdm(range(0, render_cameras.shape[1], chunk_size)):
+        if is_flexicubes:
+            frame = model.forward_geometry(
+                planes,
+                render_cameras[:, i:i+chunk_size],
+                render_size=render_size,
+            )['img']
+        else:
+            frame = model.forward_synthesizer(
+                planes,
+                render_cameras[:, i:i+chunk_size],
+                render_size=render_size,
+            )['images_rgb']
+        frames.append(frame)
+    
+    frames = torch.cat(frames, dim=1)[0]    # we suppose batch size is always 1
+    return frames
+
+# Stage 0: Configuration.
+config_path = instantmesh_base / "configs" / "instant-mesh-base.yaml"
+config = OmegaConf.load(config_path)
+config_name = os.path.basename(config_path).replace('.yaml', '')
+model_config = config.model_config
+infer_config = config.infer_config
+
+IS_FLEXICUBES = True if config_name.startswith('instant-mesh') else False
+
+device = torch.device('cuda')
+
+# load diffusion model
+print('Loading diffusion model ...')
+pipeline = DiffusionPipeline.from_pretrained(
+    "sudo-ai/zero123plus-v1.2", 
+    custom_pipeline=str(instantmesh_base / "zero123plus"),
+    torch_dtype=torch.float16,
+)
+pipeline.scheduler = EulerAncestralDiscreteScheduler.from_config(
+    pipeline.scheduler.config, timestep_spacing='trailing'
+)
+
+# load custom white-background UNet
+print('Loading custom white-background unet ...')
+if os.path.exists(infer_config.unet_path):
+    unet_ckpt_path = infer_config.unet_path
+else:
+    unet_ckpt_path = hf_hub_download(repo_id="TencentARC/InstantMesh", filename="diffusion_pytorch_model.bin", repo_type="model")
+state_dict = torch.load(unet_ckpt_path, map_location='cpu')
+pipeline.unet.load_state_dict(state_dict, strict=True)
+
+pipeline = pipeline.to(device)
+
+# load reconstruction model
+print('Loading reconstruction model ...')
+model = instantiate_from_config(model_config)
+if os.path.exists(infer_config.model_path):
+    model_ckpt_path = infer_config.model_path
+else:
+    model_ckpt_path = hf_hub_download(repo_id="TencentARC/InstantMesh", filename=f"{config_name.replace('-', '_')}.ckpt", repo_type="model")
+state_dict = torch.load(model_ckpt_path, map_location='cpu')['state_dict']
+state_dict = {k[14:]: v for k, v in state_dict.items() if k.startswith('lrm_generator.')}
+model.load_state_dict(state_dict, strict=True)
+if torch.cuda.device_count() > 1:
+    print(f"Using {torch.cuda.device_count()} GPUs")
+    model = torch.nn.DataParallel(model)
+model = model.to(device)
+if IS_FLEXICUBES:
+    model.init_flexicubes_geometry(device, fovy=30.0)
+
+model = model.eval()
+
+# make output directory
+output_mesh_path = output_base / "meshes"
+os.makedirs(output_mesh_path, exist_ok=True)
+
+# Extract all input files from input directory
+input_files = [
+        os.path.join(input_base, file) 
+        for file in os.listdir(input_base) 
+        if file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg')
+    ]
+print(f'Total number of input images: {len(input_files)}')
+
+# Stage 1: Multiview generation.
+
+# load rembg
+bg_remover = rembg.new_session()
+
+outputs = []
+for idx, image_file in enumerate(input_files):
+    name = os.path.basename(image_file).split('.')[0]
+    print(f'[{idx+1}/{len(input_files)}] Imagining {name} ...')
+
+    # remove background optionally
+    input_image = Image.open(image_file)
+    input_image = remove_background(input_image, bg_remover)
+    input_image = resize_foreground(input_image, 0.85)
+    
+    # sampling
+    output_image = pipeline(
+        input_image, 
+        num_inference_steps=75,
+    ).images[0]
+
+    images = np.asarray(output_image, dtype=np.float32) / 255.0
+    images = torch.from_numpy(images).permute(2, 0, 1).contiguous().float()     # (3, 960, 640)
+    images = rearrange(images, 'c (n h) (m w) -> (n m) c h w', n=3, m=2)        # (6, 3, 320, 320)
+
+    outputs.append({'name': name, 'images': images})
+
+# delete pipeline to save memory
+del pipeline
+
+# Stage 2: Reconstruction.
+input_cameras = get_zero123plus_input_cameras(batch_size=1, radius=4.0).to(device)
+chunk_size = 20 if IS_FLEXICUBES else 1
+
+for idx, sample in enumerate(outputs):
+    name = sample['name']
+    print(f'[{idx+1}/{len(outputs)}] Creating {name} ...')
+
+    images = sample['images'].unsqueeze(0).to(device)
+    images = v2.functional.resize(images, 320, interpolation=3, antialias=True).clamp(0, 1)
+
+    with torch.no_grad():
+        # get triplane
+        planes = model.forward_planes(images, input_cameras)
+
+        # get mesh
+        mesh_path_idx = os.path.join(output_mesh_path, f'{name}.obj')
+
+        mesh_out = model.extract_mesh(
+            planes,
+            use_texture_map=create_texture_map,
+            **infer_config,
+        )
+        if create_texture_map:
+            vertices, faces, uvs, mesh_tex_idx, tex_map = mesh_out
+            save_obj_with_mtl(
+                vertices.data.cpu().numpy(),
+                uvs.data.cpu().numpy(),
+                faces.data.cpu().numpy(),
+                mesh_tex_idx.data.cpu().numpy(),
+                tex_map.permute(1, 2, 0).data.cpu().numpy(),
+                mesh_path_idx
+            )
+        else:
+            vertices, faces, vertex_colors = mesh_out
+            save_obj(vertices, faces, vertex_colors, mesh_path_idx)
+        print(f"Mesh saved to {mesh_path_idx}")
